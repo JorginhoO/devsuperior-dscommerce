70c9622940ee95a5de5229ece4124c9eb6ca5f4e
JorginhoO <jorge.info88@gmail.com>
2024-07-01 23:39:57 -0300

    Bean Validation

---
+++ src/main/java/com/devsuperior/dscommerce/dto/FildMessage.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dscommerce.dto;
+
+public class FildMessage {
+
+    private String fieldName;
+    private String message;
+
+    public FildMessage(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
